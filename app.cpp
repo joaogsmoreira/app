3b6c7955a2db9931306b3c7464a62eb7e865a60b
joaogsmoreira <joaogsmoreira@outlook.com>
2022-01-12 16:56:10 +0000

    Added matrix multiplication

--- app.cpp
+++ app.cpp
@@ -1,15 +1,25 @@
-#include <iostream>
 #include "math/math.h"
 #include "time/time.h"
 
-#define ROWS     10
-#define COLS     10
-
 int main(void) {
-    Timer t("matrix");
-    t.start();
-    Matrix<float> m(ROWS, COLS);
-    t.stop();
-    m.print();
-    t.print();
+    Timer timerA("A");
+    Timer timerB("B");
+
+    timerA.start();
+    Matrix<int> a(3, 2);
+    timerA.stop();
+    timerA.print();
+    std::cout << "Matrix A:" << std::endl;
+    a.print();
+
+    timerB.start();
+    Matrix<int> b(2, 3);
+    timerB.stop();
+    timerB.print();
+    std::cout << "Matrix B:" << std::endl;
+    b.print();
+
+    Matrix<int> c = matrix_multiplier(a, b);
+    std::cout << "Matrix C:" << std::endl;
+    c.print();
 }
