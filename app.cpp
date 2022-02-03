85b86ff5b311e39cc8fcefce9603d640274e93c0
joaogsmoreira <joaogsmoreira@outlook.com>
2022-02-03 17:47:55 +0000

    Loading label and images

--- app.cpp
+++ app.cpp
@@ -1,24 +1,12 @@
 #include "math/matrix.h"
 #include "math/load.h"
+#include "math/functions.h"
 #include "time/time.h"
 
 int main(void) {
-    Timer timer("Multiplication Time");
+  Timer timer("Main running time");
 
-    Matrix<float> a(1000, 1000, true);
-    a = loadMatrixFromFile<float>("/home/johny/neural-network/examples/m1000x1000/matrixA.tabseparated");
-    //std::cout << "Matrix A:" << std::endl;
-    //a.print();
-
-    Matrix<float> b(1000, 1000, true);
-    b = loadMatrixFromFile<float>("/home/johny/neural-network/examples/m1000x1000/matrixB.tabseparated");
-    //std::cout << "Matrix B:" << std::endl;
-    //b.print();
-
-    timer.start();
-    Matrix<float> c = matrixMultiplier(a, b);
-    timer.stop();
-    //std::cout << "Matrix C:" << std::endl;
-    //c.print();
-    timer.print();
-}
+  load::IdxLoader bd;
+  bd.Label("/home/johny/neural-network/examples/train-labels.idx1-ubyte");
+  bd.Image("/home/johny/neural-network/examples/train-images.idx3-ubyte");
+} 
